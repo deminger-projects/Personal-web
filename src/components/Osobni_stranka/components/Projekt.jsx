d43d51acf06278f0f89a48aac5a1d3c618f4c424
pikosoZBrna <dominikjan1@gmail.com>
2024-11-01 19:09:21 +0100

    video play update

--- src/components/Osobni_stranka/components/Projekt.jsx
+++ src/components/Osobni_stranka/components/Projekt.jsx
@@ -11,7 +11,7 @@ export default function Projekt(props){
                 <div className="project_item is_left">
                         <div className="video_div">
                      
-                            <video muted src={props.video_url} onMouseOver={event => event.target.play()} onMouseOut={event => event.target.pause()}></video>
+                            <video muted src={props.video_url} onMouseOver={event => {if(!(event.target.currentTime > 0 && !event.target.paused && event.target.readyState > event.target.HAVE_CURRENT_DATA)){event.target.play()}}} onMouseOut={event => event.target.pause()}></video>
                         </div>
                     
                         <div className="text_div">
